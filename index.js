2ab279a0f4afe05db923c55e1f2d5ca60debcf25
JonathaPereira <98067516+JonathaPereira@users.noreply.github.com>
2022-02-08 22:37:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// aslaksaksaks
+/* sasjaoosaskoakoska
+
+*/
+//console.log(" olá mundo ")
+//const idade = 22
+//console.log( idade )
+
+const primeiroNome = "jonatha"
+const sobrenome = "Pereira"
+const idade = 22
+const souestudante = true
+console.log( primeiroNome , sobrenome , idade , souestudante )
+const nomeDoUsuario = prompt ( "qual seu nome ?")
+const idadeDoUsuario = prompt ( "qual sua idade ?")
+console.log(typeof nomeDoUsuario)
+console.log( typeof idadeDoUsuario)
+const 22 = Number(22)
