aec5d7c5ec9dc8247f4ce4d63bd2f9ca9e3e8eb1
Jonatha Pereira <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>
2022-02-14 23:35:05 -0300

    atividade de strings

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,43 @@
+//false
+//false
+//true
+//boolean
+let primeiroNumero = 5
+let segundoNumero = 6
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+56
+//se retirar o prompt, a soma sera feita
+
+//exercicio de codigo
+const jonatha = 22
+console.log(jonatha)
+const idadeamigo = 21
+console.log(idadeamigo)
+console.log(jonatha>idadeamigo)
+//true//
+const par = 2
+const restodadivisao = 2 % 2
+console.log(restodadivisao) //0
+
+const idadeanos = 22
+const idademeses = 264
+const idadehoras = 190080
+
+console.log(idadeanos)
+console.log(idademeses)
+console.log(idadehoras)
+
+//4
+
+const primeironumero = 4
+const segundonumero = 2
+
+console.log(primeironumero>segundonumero) //true
+console.log(segundonumero>primeironumero) //false
+console.log(primeironumero/segundonumero) // sim
+console.log(segundonumero/primeironumero) // sim
+
+
